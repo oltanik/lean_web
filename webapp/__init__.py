eee1f081b00c1de5f30e525fbe7dc3b6b04655a7
i.tolstikhin <i.tolstikhin@rabota.ru>
2023-12-15 14:27:19 +0300

    исправлен create_db.py

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 from webapp.model import db
 from webapp.python_org_news import get_python_news
 from webapp.weather import weather_by_city
+
 
 def create_app():
     app = Flask(__name__)
@@ -15,6 +16,5 @@ def create_app():
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
         news_list = get_python_news()
         return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
-    
-    return app
 
+    return app
--- webapp/config.py
+++ webapp/config.py
@@ -6,4 +6,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 WEATHER_DEFAULT_CITY = 'Rostov-on-Don,Russia'
 WEATHER_API_KEY = '69455130de1d423eb78130555230412'
 WEATHER_URL = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
-SQLALCHEMY_DATABASE_URL = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
